5c6cfdcd86f9c98e23552c6966c396aa9924cc33
Majid Khonji <majid.khonji@gmail.com>
2024-05-29 04:23:20 +0400

    fixed doller sign issue

--- functions/gptsec.fish
+++ functions/gptsec.fish
@@ -2,6 +2,7 @@ function gptsec
     # below is the gpt model
     set -l ret (echo -ne "\r")
     set -l tab (echo -ne "\t")
+   
 
     if test "$argv[1]" = "ask"
         commandline -i "sgpt --chat $chat_history --model $model \"\""
@@ -9,12 +10,14 @@ function gptsec
         commandline -C (math $len - 1)
 
     else if test "$argv[1]" = "check"
-        set text $text (xclip -o -selection primary | tr '\\n' ' ' | sed 's/\\"/\\\\"/g') 
+        #set text $text (xclip -o -selection primary | tr '\\n' ' ' | sed 's/\\"/\\\\"/g') 
+        set text $text(xclip -o -selection primary | tr '\\n' ' ' | sed 's/\\"/\\\\"/g; s/\$/\\\$/g') 
         sgpt --chat $chat_history --model $model $text
 
     else if test "$argv[1]" = "inline"
         set -l text "Below is the concerned text of the question below. Be concise in your answer.\n\n########\nTEXT:\n--\n\n"
-        set text $text(xclip -o -selection primary | tr '\\n' ' ' | sed 's/\\"/\\\\"/g') 
+        #set text $text(xclip -o -selection primary | tr '\\n' ' ' | sed 's/\\"/\\\\"/g') 
+        set text $text(xclip -o -selection primary | tr '\\n' ' ' | sed 's/\\"/\\\\"/g; s/\$/\\\$/g') 
         set text $text "\n\n########\n\nQUESTION:\n--\n\n"
       
 
